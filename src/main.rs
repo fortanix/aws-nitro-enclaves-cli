eaacd8c2fe8cd3447a3f942dba883d587fb5110e
Raoul Strackx <raoul.strackx@fortanix.com>
2022-11-28 17:41:22 +0100

    Move `create_enclave` to lib

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ use nitro_cli::common::commands_parser::{
     TerminateEnclavesArgs,
 };
 use nitro_cli::common::document_errors::explain_error;
-use nitro_cli::common::json_output::{EnclaveBuildInfo, EnclaveDescribeInfo, EnclaveRunInfo, EnclaveTerminateInfo};
+use nitro_cli::common::json_output::{EnclaveBuildInfo, EnclaveDescribeInfo, EnclaveTerminateInfo};
 use nitro_cli::common::{
     enclave_proc_command_send_single, logger, NitroCliErrorEnum, NitroCliFailure, NitroCliResult,
 };
@@ -25,14 +25,13 @@ use nitro_cli::common::{EnclaveProcessCommandType, ExitGracefully};
 use nitro_cli::enclave_proc::resource_manager::NE_ENCLAVE_DEBUG_MODE;
 use nitro_cli::enclave_proc_comm::{
     enclave_proc_command_send_all, enclave_proc_connect_to_single, enclave_proc_get_cid,
-    enclave_proc_get_flags, enclave_proc_spawn, enclave_process_handle_all_replies,
+    enclave_proc_get_flags, enclave_process_handle_all_replies,
 };
 use nitro_cli::{
-    build_enclaves, console_enclaves, create_app, describe_eif, get_all_enclave_names,
-    get_file_pcr, new_enclave_name, new_nitro_cli_failure, terminate_all_enclaves,
+    build_enclaves, console_enclaves, create_app, describe_eif,
+    get_file_pcr, new_nitro_cli_failure, RUN_ENCLAVE_STR, terminate_all_enclaves,
 };
 
-const RUN_ENCLAVE_STR: &str = "Run Enclave";
 const DESCRIBE_ENCLAVE_STR: &str = "Describe Enclave";
 const DESCRIBE_EIF_STR: &str = "Describe EIF";
 const TERMINATE_ENCLAVE_STR: &str = "Terminate Enclave";
@@ -40,7 +39,6 @@ const TERMINATE_ALL_ENCLAVES_STR: &str = "Terminate All Enclaves";
 const BUILD_ENCLAVE_STR: &str = "Build Enclave";
 const ENCLAVE_CONSOLE_STR: &str = "Enclave Console";
 const EXPLAIN_ERR_STR: &str = "Explain Error";
-const NEW_NAME_STR: &str = "New Enclave Name";
 const FILE_PCR_STR: &str = "File PCR";
 
 /// *Nitro CLI* application entry point.
@@ -73,52 +71,13 @@ fn main() {
 
     match args.subcommand() {
         ("run-enclave", Some(args)) => {
-            let mut run_args = RunEnclavesArgs::new_with(args)
+            let run_args = RunEnclavesArgs::new_with(args)
                 .map_err(|err| {
                     err.add_subaction("Failed to construct RunEnclave arguments".to_string())
                         .set_action(RUN_ENCLAVE_STR.to_string())
                 })
                 .ok_or_exit_with_errno(None);
-            let mut comm = enclave_proc_spawn(&logger)
-                .map_err(|err| {
-                    err.add_subaction("Failed to spawn enclave process".to_string())
-                        .set_action(RUN_ENCLAVE_STR.to_string())
-                })
-                .ok_or_exit_with_errno(None);
-
-            let names = get_all_enclave_names()
-                .map_err(|e| {
-                    e.add_subaction("Failed to handle all enclave process replies".to_string())
-                        .set_action("Get Enclaves Name".to_string())
-                })
-                .ok_or_exit_with_errno(None);
-            run_args.enclave_name = Some(
-                new_enclave_name(run_args.clone(), names)
-                    .map_err(|err| {
-                        err.add_subaction("Failed to assign a new enclave name".to_string())
-                            .set_action(NEW_NAME_STR.to_string())
-                    })
-                    .ok_or_exit_with_errno(None),
-            );
-
-            enclave_proc_command_send_single(
-                EnclaveProcessCommandType::Run,
-                Some(&run_args),
-                &mut comm,
-            )
-            .map_err(|e| {
-                e.add_subaction("Failed to send single command".to_string())
-                    .set_action(RUN_ENCLAVE_STR.to_string())
-            })
-            .ok_or_exit_with_errno(None);
-
-            info!("Sent command: Run");
-            replies.push(comm);
-            enclave_process_handle_all_replies::<EnclaveRunInfo>(&mut replies, 0, false, vec![0])
-                .map_err(|e| {
-                    e.add_subaction("Failed to handle all enclave process replies".to_string())
-                        .set_action(RUN_ENCLAVE_STR.to_string())
-                })
+            nitro_cli::create_enclave(run_args, &logger)
                 .ok_or_exit_with_errno(None);
         }
         ("terminate-enclave", Some(args)) => {
